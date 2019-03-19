443b0e5708f8f98c63359055ca135603a98c926a
TaviusC <47620551+TaviusC@users.noreply.github.com>
2019-03-19 00:38:08 -0400

    Add files via upload
    Program for calculating the amount of calories burned for a set amount of minutes.

---
+++ P4HW1_CaloriesBurned_TaviusCousar.py
@@ -0,0 +1,25 @@
+# This program caculates the amount of calories burned
+# after a set amount of minutes.
+# 3/18/2019
+# CTI-110 P4HW1 - Calories Burned
+# Tavius Cousar
+
+# Create for loop for calories burned
+# The assumed amount of calories burned is 5 per minute
+# The num amounts are 20, 35, and 45
+# Multiply num by 5
+# display amount of calories burned
+
+# Clarify amount of calories burned per minute.
+print("Assuming you've burned 5 calories per minute: ")
+
+# Set up the for loop.
+for num in [20, 35, 45]:
+
+    # Multiply "num" by 5.
+    # Display amount of calories burned.
+    print('Calories burned after', num, 'minutes is: ', num * 5)
+
+    
+    
+
